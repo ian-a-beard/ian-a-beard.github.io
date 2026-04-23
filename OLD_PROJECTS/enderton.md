cb9e4135f64d8c967535c5d60de4d5085b306d18
Ian Beard <iab273@nyu.edu>
2026-04-23 10:26:10 -0700

    new website structure

--- OLD_PROJECTS/enderton.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: Enderton Logic Solutions
-description: My solutions to Enderton's book "Introduction to mathematical logic"
-importance: 1
-category: math
-redirect: /assets/pdf/enderton-solutions.pdf
----
--- blog-post-tools/copy_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make target folder if it doesn't exist
-mkdir -p marfa-selections
-
-# Read each code from codes.txt and copy matching files
-while read code; do
-    cp IMG_"$code".JPG marfa-selections/ 2>/dev/null
-done < codes.txt
